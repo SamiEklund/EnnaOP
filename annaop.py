7a95746f322fa573a57d5f8ea1b2206d31b81d02
Sami Eklund <sami_eklund94@hotmail.com>
2016-10-29 19:20:19 +0300

    Added checking for new releases Added utf-8 encoding to the script Added IRC message structure

--- annaop.py
+++ annaop.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import feedparser, jsonpickle
 
 class Manga:
@@ -18,15 +19,18 @@ class Manga:
     def toString(self):
         return self.name + " No. " + str(self.chapterNumber) + " - " + self.chapterTitle + " | " + self.publishedDate
 
+    def getMessage(self):
+        return [self.mangaName + " " + str(self.chapterNumber) + " is out! " + " ".join(self.followers), self.chapterLink]
+
 class MangaParser:
     mangaList = None
     mangaFile = "manga.txt"
 
     def __init__(self):
         self.loadFromFile()
-
-        for manga in self.mangaList:
-            print(manga.mangaName)
+        releases = self.checkForNewReleases(self.parseMangaStream())
+        for msg in releases:
+            print(msg[0] + "\n" + msg[1])
 
     def parseMangaStream(self):
         feed = feedparser.parse("http://mangastream.com/rss")
@@ -50,23 +54,45 @@ class MangaParser:
             try:
                 chapterNumber = int(chapterNumber)
             except:
-                print("Entry " + mangaName + " has no chapter number! Skipping it!")
                 continue
 
             feedMangas.append(Manga(mangaName, chapterNumber, chapterTitle, chapterLink, publishedDate))
         return feedMangas
 
+    def checkForNewReleases(self, releases):
+        newReleases = []
+        ircMessages = []
+
+        for release in releases:
+            try:
+                manga = self.mangaList[release.mangaName]
+            except:
+                continue
+            if release.chapterNumber > manga.chapterNumber:
+                newReleases.append(release)
+
+        for release in newReleases:
+            manga = self.mangaList[release.mangaName]
+            ircMessages.append(release.getMessage())
+            if manga and release.chapterNumber > manga.chapterNumber:
+                self.mangaList[release.mangaName] = release
+        
+        self.saveToFile()
+        return ircMessages
+        
+
     def saveToFile(self):
         file = open(self.mangaFile, "w")
         for manga in self.mangaList:
-            file.write(jsonpickle.encode(manga) + "\n")
+            file.write(jsonpickle.encode(self.mangaList[manga]) + "\n")
         file.close()
 
     def loadFromFile(self):
-        self.mangaList = []
+        self.mangaList = {}
         file = open(self.mangaFile, "r")
         for line in file:
-            self.mangaList.append(jsonpickle.decode(line))
+            manga = jsonpickle.decode(line)
+            self.mangaList[manga.mangaName] = manga
         file.close()
 
 
